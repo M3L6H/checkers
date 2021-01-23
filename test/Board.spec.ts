c788cdad22a12de52a3de36ff0d605112762bccc
M3L6H <sisoac@hotmail.com>
2021-01-23 00:12:55 -0800

    Add spec for #render

--- test/Board.spec.ts
+++ test/Board.spec.ts
@@ -1,14 +1,30 @@
 import { Board } from "../src";
 import { Color } from "../src/pieces";
 import { expect } from "chai";
+import { JSDOM } from "jsdom";
 import "mocha";
+
+const loopThroughBoardAnd = (cb: (i: number, j: number) => unknown) => {
+  for (let i = 0; i < Board.SIZE; ++i)
+    for (let j = 0; j < Board.SIZE; ++j) cb(i, j);
+};
 
 describe("Board", () => {
   let board: Board;
+  let dom: JSDOM;
+  let document: Document;
 
-  beforeEach(() => board = new Board());
+  before(() => {
+    dom = new JSDOM(`<!DOCTYPE html>
+      <p>Hello World</p>
+    `);
+    document = dom.window.document;
+    global["document"] = document;
+  });
   
-  describe ("#pieceAt", () => {
+  beforeEach(() => board = new Board());
+
+  describe("#pieceAt", () => {
     it("should return the piece at the given position", () => {
       for (let i = 4; i <= 5; ++i) {
         for (let j = 0; j < 8; ++j)
@@ -21,6 +37,33 @@ describe("Board", () => {
       expect(() => board.pieceAt(0, -1)).to.throw(RangeError);
       expect(() => board.pieceAt(Board.SIZE, 0)).to.throw(RangeError);
       expect(() => board.pieceAt(0, Board.SIZE)).to.throw(RangeError);
+    });
+  });
+
+  describe("#render", () => {
+    let rootEle: HTMLDivElement;
+    
+    beforeEach(() => rootEle = document.createElement("div"));
+    
+    it(`should create an ${ Board.SIZE }x${ Board.SIZE } grid of tiles`, () => {
+      board.render(rootEle);
+
+      loopThroughBoardAnd((i, j) => {
+        const tile = rootEle.children[i].children[j];
+        expect(tile.classList.contains("tile")).to.be.true;
+      });
+    });
+
+    describe("when flipped", () => {
+      it(`should render 0,0 in the top right and ${ Board.SIZE - 1 }, ${ Board.SIZE - 1 } in the bottom left`, () => {
+        board.render(rootEle, { flipped: true });
+
+        loopThroughBoardAnd((i, j) => {
+          const tile = rootEle.children[i].children[j] as HTMLDivElement;
+          expect(tile.dataset.row).to.equal(i.toString());
+          expect(tile.dataset.col).to.equal((Board.SIZE - j - 1).toString());
+        });
+      });
     });
   });
 });
