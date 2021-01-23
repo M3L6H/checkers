93e2732663204f6cd2367f860a8f902ac67f8279
M3L6H <sisoac@hotmail.com>
2021-01-22 21:33:19 -0800

    Add null piece

---
+++ src/NullPiece.ts
@@ -0,0 +1,14 @@
+import Color from "./Color";
+import Piece from "./Piece";
+
+export default class NullPiece implements Piece {
+  readonly color: Color;
+  
+  constructor() {
+    this.color = Color.Null;
+  }
+
+  isEmpty() {
+    return true;
+  }
+}
