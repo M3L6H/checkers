de76f8f69da42a130614f1c84f156df4e539efe8
M3L6H <sisoac@hotmail.com>
2021-01-23 12:41:51 -0800

    Add rules for sass

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,14 @@ module.exports = {
         test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-laoder"
+        ]
       }
     ]
   },
