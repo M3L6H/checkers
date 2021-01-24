9f4a5a815494434c67ede6e0fea5f2309a7f272a
M3L6H <sisoac@hotmail.com>
2021-01-24 00:57:06 -0800

    Add documentation to Color enum

--- src/pieces/Color.ts
+++ src/pieces/Color.ts
@@ -1,3 +1,11 @@
+/**
+ * Enum for the possible piece colors.
+ * Has no reflection on the actual appearance of the pieces.
+ * @category Types
+ * @property {number} Null  - Used by the {@link NullPiece}
+ * @property {number} Black - Color for the black pieces
+ * @property {number} Red   - Color for the red pieces
+ */
 enum Color {
   Null,
   Black,
