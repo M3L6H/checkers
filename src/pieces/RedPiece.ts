1c2167eb3ea9ba6afe6407f2d94bb410a852a26f
M3L6H <sisoac@hotmail.com>
2021-01-24 02:13:56 -0800

    Add documentation for RedPiece

--- src/pieces/RedPiece.ts
+++ src/pieces/RedPiece.ts
@@ -1,7 +1,20 @@
 import Color from "./Color";
 import Piece from "./Piece";
 
+/**
+ * Represents a red piece on the {@link Board}.
+ * 
+ * @category Pieces
+ * @extends Piece
+ */
 class RedPiece extends Piece {
+
+  /**
+   * The color of a red piece is always {@link Color}.Red.
+   * 
+   * @readonly
+   * @property {Color} color  - Always {@link Color}.Red
+   */
   readonly color: Color;
 
   constructor() {
