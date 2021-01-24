3e3380de517da8ff5718eac76c72ddc68039acf0
M3L6H <sisoac@hotmail.com>
2021-01-24 01:51:59 -0800

    Add documentation for BlackPiece class

--- src/pieces/BlackPiece.ts
+++ src/pieces/BlackPiece.ts
@@ -1,7 +1,20 @@
 import Color from "./Color";
 import Piece from "./Piece";
 
+/**
+ * Represents a black piece on the {@link Board}.
+ * 
+ * @category Pieces
+ * @implements {@link Piece}
+ */
 class BlackPiece implements Piece {
+
+  /**
+   * The color of a black piece is always {@link Color}.Black.
+   * 
+   * @readonly
+   * @property {Color} color  - Always {@link Color}.Black
+   */
   readonly color: Color;
 
   private _isKing: boolean;
