b4449777530fc57e395dfda91027268aa345fd35
M3L6H <sisoac@hotmail.com>
2021-01-24 01:04:46 -0800

    Add documentation for the Piece interface

--- src/pieces/Piece.ts
+++ src/pieces/Piece.ts
@@ -1,8 +1,30 @@
 import Color from "./Color";
 
+/**
+ * Defines the interface of a checkers piece.
+ * 
+ * @category Pieces
+ */
 interface Piece {
+
+  /**
+   * The {@link Color} of a piece.
+   * @readonly
+   */
   readonly color: Color;
+
+  /**
+   * Whether or not the piece is a king.
+   * @readonly
+   */
   readonly isKing: boolean;
+
+  /**
+   * Promotes the piece into a King, if applicable.
+   * 
+   * @method promote
+   * @see NullPiece
+   */
   promote(): void;
 }
 
