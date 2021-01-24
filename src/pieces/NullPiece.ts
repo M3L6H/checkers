d811e60680ba4044bf306ff519a2a35fb6c4076d
M3L6H <sisoac@hotmail.com>
2021-01-24 01:35:25 -0800

    Add documentation for null piece

--- src/pieces/NullPiece.ts
+++ src/pieces/NullPiece.ts
@@ -1,23 +1,43 @@
 import Color from "./Color";
 import Piece from "./Piece";
 
+/**
+ * Represents an empty square on the {@link Board}.
+ * 
+ * @category Pieces
+ * @implements {@link Piece}
+ */
 class NullPiece implements Piece {
+
+  /**
+   * The color of the NullPiece is always {@link Color}.Null.
+   * 
+   * @readonly
+   * @property {Color} color  - Always {@link Color}.Null
+   */
   readonly color: Color;
 
-  private _isKing: boolean;
-  
+  /**
+   * Creates a new NullPiece.
+   */
   constructor() {
     this.color = Color.Null;
-    this._isKing = false;
   }
 
-  get isKing(): boolean {
-    return this._isKing;
-  }
+  /**
+   * Getter for the isKing property. Always returns false, since a NullPiece
+   * cannot be a "king."
+   * 
+   * @readonly
+   * @property {boolean} isKing - Always false
+   */
+  get isKing(): boolean { return false; } // eslint-disable-line
 
-  promote(): void {
-    this._isKing = false;
-  }
+  /**
+   * Method stub implemented for the {@link Piece} interface.
+   * Does nothing, since a NullPiece cannot promote.
+   */
+  public promote(): void { } // eslint-disable-line
 }
 
 export default NullPiece;
