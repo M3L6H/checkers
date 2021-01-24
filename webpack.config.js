7001f24bb8e25341fd5178cba4809040da648107
M3L6H <sisoac@hotmail.com>
2021-01-24 15:15:31 -0800

    Update output directory of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
   },
   output: {
     filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist/frontend/")
   },
   optimization: {
     moduleIds: "deterministic",
