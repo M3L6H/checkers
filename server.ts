c00e8d1f10f997c068cd3b5967e4272637e9d9e4
M3L6H <sisoac@hotmail.com>
2021-01-24 19:59:43 -0800

    Add livereloading with nodemon and livereload

--- server.ts
+++ server.ts
@@ -4,6 +4,20 @@ import path from "path";
 const app = express();
 const port = 8080;
 
+// Set up livereload middleware
+// eslint-disable-next-line
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+// eslint-disable-next-line
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
 // Configure Express to use EJS
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
