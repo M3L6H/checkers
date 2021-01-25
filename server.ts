6bcb1ca267df19a78613184cb894195fcd7992d5
M3L6H <sisoac@hotmail.com>
2021-01-24 18:25:09 -0800

    Update server with views configuration

--- server.ts
+++ server.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = 8080;
 
+// Configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Route handler for home page
 app.get("/", (_req, res) => {
-  res.send("Hello world!");
+  res.render("index");
 });
 
 process.on("SIGINT", () => process.exit());
