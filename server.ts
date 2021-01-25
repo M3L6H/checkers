0b8ca267bdf375981bd345aba211bde3d89f5ccd
M3L6H <sisoac@hotmail.com>
2021-01-24 16:20:24 -0800

    Add SIGINT listener to kill server in Docker container

--- server.ts
+++ server.ts
@@ -3,9 +3,11 @@ import express from "express";
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Hello world!");
 });
+
+process.on("SIGINT", () => process.exit());
 
 app.listen(port, () => {
   // eslint-disable-next-line
