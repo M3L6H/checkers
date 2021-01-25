ebcc2fe2ad396dad0251fc556824473200418cac
M3L6H <sisoac@hotmail.com>
2021-01-24 23:27:17 -0800

    Use env variable to configure livereload

--- server.ts
+++ server.ts
@@ -1,22 +1,28 @@
+import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+
+// Initialize configuration
+dotenv.config();
 
 const app = express();
 const port = 8080;
 
-// Set up livereload middleware
-// eslint-disable-next-line
-const livereload = require("livereload");
-const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
-});
-
-// eslint-disable-next-line
-const connectLivereload = require("connect-livereload");
-app.use(connectLivereload());
+if (process.env.NODE_ENV === "development") {
+  // Set up livereload middleware
+  // eslint-disable-next-line
+  const livereload = require("livereload");
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+  
+  // eslint-disable-next-line
+  const connectLivereload = require("connect-livereload");
+  app.use(connectLivereload());
+}
 
 // Configure Express to use EJS
 app.set("views", path.join(__dirname, "views"));
