0ca305c2f39207fefc0abc711820e547b34b60cd
M3L6H <sisoac@hotmail.com>
2021-01-25 23:07:32 -0800

    Restructuring app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-export { default as Board } from "./Board";
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { setupLivereload } from "./middleware";
+import { HelloResolver } from "./resolvers";
+
+const main = async () => {
+  const app = express();
+  const port = process.env.PORT || 8080;
+
+  // Use livereload if in development
+  if (process.env.NODE_ENV !== "production") setupLivereload(app);
+  
+  // Set up apollo server
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
+
+  apolloServer.applyMiddleware({ app });
+  
+  // Exit when SIGINT is received
+  process.on("SIGINT", () => process.exit());
+  
+  app.listen(port, () => {
+    // eslint-disable-next-line
+    console.log(`Server started on ${ port }`);
+  });
+};
+
+main();
